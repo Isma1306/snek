6e801c62e31f806d01bf4c6970f142bd36ca3717
Isma1306 <isma1306@gmail.com>
2024-04-12 21:34:06 +0200

    now the client use go channels to talk with the game WIP

--- main.go
+++ main.go
@@ -12,48 +12,90 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+type Client struct {
+	Conn      *websocket.Conn
+	Direction string
+}
+
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
 }
-
-type Message struct {
-	Username string `json:"username"`
-	Message  string `json:"message"`
-}
-
-var clients = make(map[*websocket.Conn]bool)
-var broadcast = make(chan Message)
+var game = new(Game)
+var clients = make(map[*websocket.Conn]*Client)
+var broadcast = make(chan string)
 
 type Res struct {
 	Direction string `json:"direction"`
 }
 
 func main() {
-	http.HandleFunc("/game", func(w http.ResponseWriter, r *http.Request) {
 
+	http.HandleFunc("/game", func(w http.ResponseWriter, r *http.Request) {
 		conn, err := upgrader.Upgrade(w, r, nil)
 		if err != nil {
 			log.Printf("Error upgrading: %s", err)
 		}
+		client := Client{Conn: conn, Direction: "right"}
+
+		clients[conn] = &client
 		defer conn.Close()
 		conn.SetCloseHandler(func(code int, text string) error {
 			log.Printf("connection lost with client: %s", conn.RemoteAddr())
 			return fmt.Errorf("connection close")
 		})
-		game := new(Game)
+
+		appleTemplate := Render("apple", game.Apple)
+		broadcastTmpl(appleTemplate.Bytes())
+
+		go func() {
+			for {
+				game.Snek.Direction = <-broadcast
+
+			}
+		}()
+
+		// game loop
+
+		// read messages
+		for {
+			_, msg, err := conn.ReadMessage()
+			if err != nil {
+				delete(clients, conn)
+				log.Println("Error reading message")
+				return
+			}
+			response := Res{}
+			err = json.Unmarshal([]byte(msg), &response)
+			if err != nil {
+				log.Println("Error parsing json")
+				return
+
+			}
+
+			log.Printf("Recieve: %s From: %s", response.Direction, conn.RemoteAddr())
+			if CheckDirection(game.Snek.Direction, "vertical") && CheckDirection(response.Direction, "horizontal") {
+				broadcast <- response.Direction
+			}
+			if CheckDirection(game.Snek.Direction, "horizontal") && CheckDirection(response.Direction, "vertical") {
+				broadcast <- response.Direction
+			}
+		}
+
+	})
+
+	http.HandleFunc("/start", func(w http.ResponseWriter, r *http.Request) {
+		tmpl := Render("start", game)
+		w.Write(tmpl.Bytes())
+
+	})
+
+	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+		game = new(Game)
 		game.newSnek()
 		game.generateBoard()
 		game.generateApple()
-		appleTemplate := Render("apple", game.Apple)
-		if err = conn.WriteMessage(websocket.TextMessage, appleTemplate.Bytes()); err != nil {
-			return
-		}
-
-		// timer
-		go timeLoop(game, conn)
-
-		// game loop
+		go timeLoop(game)
 		go func() {
 			for {
 				time.Sleep(300 * time.Millisecond)
@@ -64,19 +106,15 @@ func main() {
 				game.Snek.move(*game, eatApple)
 				if game.checkCollision(game.Snek) {
 					msg := Render("header", "You died!")
-					if err = conn.WriteMessage(websocket.TextMessage, msg.Bytes()); err != nil {
-						return
-					}
-					conn.Close()
-					return
+					broadcastTmpl(msg.Bytes())
+
 				}
 				game.generateBoard()
 				if eatApple {
 					game.Score += 100
 					score := Render("score", game.Score)
-					if err = conn.WriteMessage(websocket.TextMessage, score.Bytes()); err != nil {
-						return
-					}
+					broadcastTmpl(score.Bytes())
+
 					game.generateApple()
 					newApple := Render("apple", game.Apple)
 					templateToRender = append(templateToRender, newApple.Bytes()...)
@@ -86,41 +124,9 @@ func main() {
 				}
 				snekTemplate := Render("apple", game.Snek.Body[0])
 				templateToRender = append(templateToRender, snekTemplate.Bytes()...)
-				if err = conn.WriteMessage(websocket.TextMessage, templateToRender); err != nil {
-					return
-				}
+				broadcastTmpl(templateToRender)
 			}
 		}()
-
-		// read messages
-		for {
-			_, msg, err := conn.ReadMessage()
-			if err != nil {
-				log.Println("Error reading message")
-				return
-			}
-			response := Res{}
-			err = json.Unmarshal([]byte(msg), &response)
-			if err != nil {
-				log.Println("Error parsing json")
-				return
-			}
-
-			log.Printf("Recieve: %s From: %s", response.Direction, conn.RemoteAddr())
-			if CheckDirection(game.Snek.Direction, "vertical") && CheckDirection(response.Direction, "horizontal") {
-				game.Snek.Direction = response.Direction
-			}
-			if CheckDirection(game.Snek.Direction, "horizontal") && CheckDirection(response.Direction, "vertical") {
-				game.Snek.Direction = response.Direction
-			}
-		}
-
-	})
-
-	http.HandleFunc("/start", func(w http.ResponseWriter, r *http.Request) {
-		game := new(Game)
-		tmpl := Render("start", game)
-		w.Write(tmpl.Bytes())
 	})
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -132,14 +138,12 @@ func main() {
 	http.ListenAndServe("0.0.0.0:10000", nil)
 }
 
-func timeLoop(game *Game, conn *websocket.Conn) {
+func timeLoop(game *Game) {
 	for {
 		time.Sleep(1 * time.Second)
 		game.Time++
-		msg := Render("time", game.Time)
-		if err := conn.WriteMessage(websocket.TextMessage, msg.Bytes()); err != nil {
-			return
-		}
+		tmpl := Render("time", game.Time)
+		broadcastTmpl(tmpl.Bytes())
 	}
 }
 
@@ -158,4 +162,15 @@ func CheckDirection(item, direction string) bool {
 		return item == "left" || item == "right"
 	}
 	return false
+}
+
+func broadcastTmpl(tmpl []byte) {
+	for client := range clients {
+		err := client.WriteMessage(websocket.TextMessage, tmpl)
+		if err != nil {
+			log.Println(err)
+			client.Close()
+			delete(clients, client)
+		}
+	}
 }
